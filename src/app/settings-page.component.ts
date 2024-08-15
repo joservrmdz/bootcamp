3c0c0518c45790d9c4a39930f29e0089b3bcd98b
José Ramón Rivera <jose.rivera@linkerfinance.com>
2024-08-15 08:30:15 -0700

    LGB-0002: feat(settings) fixed size

--- src/app/settings-page.component.ts
+++ src/app/settings-page.component.ts
@@ -3,7 +3,7 @@ import {Component} from "@angular/core";
 @Component({
     selector: 'grimlock-settings-page',
     template: `
-        <h2>Settings</h2>
+        <h2>Settings Page</h2>
     `,
     standalone: true,
 
